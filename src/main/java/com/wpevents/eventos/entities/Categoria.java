11bee6baff05e62b454e1fb6ff7c9d9e7309a1ed
Weslley Palhares <weslley_palhares@outlook.com>
2025-02-01 12:14:28 -0300

    Criação da classe  Categoria.java

---
+++ src/main/java/com/wpevents/eventos/entities/Categoria.java
@@ -0,0 +1,75 @@
+package com.wpevents.eventos.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String descricao;
+	
+	
+	public Categoria() {
+
+	}
+
+
+	public Categoria(Long id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
