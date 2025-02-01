55de6434ab07a6f8e94841399f95d9c3bf8795da
Weslley Palhares <weslley_palhares@outlook.com>
2025-02-01 12:29:44 -0300

    Criado a classe Atividade.java e os relacionamentos

--- src/main/java/com/wpevents/eventos/entities/Atividade.java
+++ src/main/java/com/wpevents/eventos/entities/Atividade.java
@@ -1,7 +1,5 @@
 package com.wpevents.eventos.entities;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -9,60 +7,76 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
+@Table(name = "tb_atividade")
+public class Atividade {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	private String nome;
+	
 	@Column(columnDefinition = "TEXT")
 	private String descricao;
 	
-	@OneToMany(mappedBy = "categoria")
-	private List<Atividade> atividades = new ArrayList<>();
+	private Double preco;
 	
-	
-	public Categoria() {
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
+	private Categoria categoria;
+
+	public Atividade() {
 
 	}
 
-
-	public Categoria(Integer id, String descricao) {
+	public Atividade(Integer id, String nome, String descricao, Double preco) {
 		this.id = id;
+		this.nome = nome;
 		this.descricao = descricao;
+		this.preco = preco;
 	}
-
 
 	public Integer getId() {
 		return id;
 	}
 
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 	public String getDescricao() {
 		return descricao;
 	}
 
-
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -72,15 +86,10 @@ public class Categoria {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Atividade other = (Atividade) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	public List<Atividade> getAtividades() {
-		return atividades;
-	}
 	
 	
 	
-
 }
